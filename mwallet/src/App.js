4c6cde324c6073d63a19b54d96b80f7c18d1c764
Raman Agrawal <iramanagrawal@gmail.com>
2024-03-17 15:52:28 +0530

    popup fix

--- mwallet/src/App.js
+++ mwallet/src/App.js
@@ -15,10 +15,27 @@ function App() {
   const [selectedChain, setSelectedChain] = useState("0x1");
   const [drop, setdrop] = useState(false);
   const [show, showKey] = useState(false);
+  const [copied, setCopied] = useState(false);
+  const privatekey = "nfkdshfkshdk";
+  const copyCodeHandler = (item) => {
+    navigator.clipboard
+      .writeText(item)
+      .then(() => {
+        // Text successfully copied
+        setCopied(() => true);
+        setTimeout(() => {
+          setCopied(true);
+        }, 2000); // Hide "Copied" message after 2 seconds
+      })
+      .catch((err) => {
+        // Handle any errors while copying
+        console.error("Failed to copy: ", err);
+      });
+  };
 
   return (
     <div className="App">
-      <header>
+      <header style={{ position: "relative" }}>
         <img src={logo} className="headerLogo" alt="logo" />
         <Select
           onChange={(val) => setSelectedChain(val)}
@@ -54,10 +71,35 @@ function App() {
         )}
         {drop && (
           <div className="popup">
-            <button type="button" onClick={() => showKey(true)}>
-             Show Private key
-            </button>
-            {show && <div> fhgfjghbhj</div>}
+            <div
+              onClick={() => {
+                setdrop(!drop);
+                showKey(false);
+              }}
+              style={{
+                position: "absolute",
+                top: "0",
+                right: "15px",
+                cursor: "pointer",
+              }}
+            >
+              X
+            </div>
+            {show ? (
+              <div>
+                <p>{privatekey}</p>
+                <button
+                  type="button"
+                  onClick={() => copyCodeHandler(privatekey)}
+                >
+                  {copied ? "Copied" : "Copy Private key"}
+                </button>
+              </div>
+            ) : (
+              <button type="button" onClick={() => showKey(true)}>
+                Show Private key
+              </button>
+            )}
           </div>
         )}
       </header>
