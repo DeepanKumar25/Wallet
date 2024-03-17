77b81c3839b5fe053794b5e1f6f1ab30a97e0dd0
Raman Agrawal <iramanagrawal@gmail.com>
2024-03-17 14:44:59 +0530

    WalletView

--- mwallet/src/components/WalletView.js
+++ mwallet/src/components/WalletView.js
@@ -167,6 +167,50 @@ function WalletView({
         </>
       ),
     },
+    {
+      key: "4",
+      label: `bridge`,
+      children: (
+        <>
+          <h3>Bridge </h3>
+          
+          <div className="sendRow">
+            <p style={{ width: "90px", textAlign: "left" }}> To:</p>
+            <Input
+              value={sendToAddress}
+              onChange={(e) => setSendToAddress(e.target.value)}
+              placeholder="0x..."
+            />
+          </div>
+          <div className="sendRow">
+            <p style={{ width: "90px", textAlign: "left" }}> Amount:</p>
+            <Input
+              value={amountToSend}
+              onChange={(e) => setAmountToSend(e.target.value)}
+              placeholder="Native tokens you wish to send..."
+            />
+          </div>
+          <Button
+            style={{ width: "100%", marginTop: "20px", marginBottom: "20px" }}
+            type="primary"
+            onClick={() => sendTransaction(sendToAddress, amountToSend)}
+          >
+            Send Tokens
+          </Button>
+          {processing && (
+            <>
+              <Spin />
+              {hash && (
+                <Tooltip title={hash}>
+                  <p>Hover For Tx Hash</p>
+                </Tooltip>
+              )}
+            </>
+          )}
+        </>
+      ),
+    },
+    
   ];
 
   async function sendTransaction(to, amount) {
